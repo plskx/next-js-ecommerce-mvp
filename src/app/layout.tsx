51b8d220011b5129fe8847cd0aee461825bd7b05
Zai Santillan <zaisantillan14@gmail.com>
2024-04-25 00:45:33 +0800

    feat: add shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
-  title: "ecommerce-mvp",
-};
+  title: 'ecommerce-mvp',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
